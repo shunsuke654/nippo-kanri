ad228ee8e5ca5b0f92261e79eed72ba53f78a14c
shunsuke.tamura <crossroad0129@gmail.com>
2021-01-19 16:52:37 +0900

    Modify top page

--- WebContent/WEB-INF/views/reports/edit.jsp
+++ WebContent/WEB-INF/views/reports/edit.jsp
@@ -5,15 +5,15 @@
         <c:choose>
             <c:when test="${report != null}">
                 <h2>日報　編集ページ</h2>
-                <form method="POST" action="<url value='/reports/update' />">
+                <form method="POST" action="<c:url value='/reports/update' />">
                     <c:import url="_form.jsp" />
                 </form>
             </c:when>
             <c:otherwise>
                 <h2>お探しのデータは見つかりませんでした。</h2>
             </c:otherwise>
-        </c:choose>>
+        </c:choose>
 
-        <p><a href="url value='/reports/index' />">一覧に戻る</a></p>
+        <p><a href="<c:url value='/reports/index' />">一覧に戻る</a></p>
     </c:param>
 </c:import>
--- src/controllers/reports/ReportsEditServlet.java
+++ src/controllers/reports/ReportsEditServlet.java
@@ -35,11 +35,11 @@ public class ReportsEditServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        Report r =em.find(Report.class, Integer.parseInt(request.getParameter("id")));
+        Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        Employee login_employee = (Employee)request.getSession().getAttribute("login_emoloyee");
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
         if(r != null && login_employee.getId() == r.getEmployee().getId()) {
             request.setAttribute("report", r);
             request.setAttribute("_token", request.getSession().getId());
